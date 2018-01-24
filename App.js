82992d6ff4ab09fd415976d69c52a939ee89cf57
Luiz Claudio <luizcns18@gmail.com>
2018-01-23 22:20:00 -0300

    feat: Add main navigator

--- App.js
+++ App.js
@@ -5,10 +5,11 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
 import History from './components/History';
-import { TabNavigator } from 'react-navigation';
+import { TabNavigator, StackNavigator } from 'react-navigation';
 import { purple, white } from './utils/colors';
 import { FontAewsome, Ionicons } from '@expo/vector-icons';
 import UdaciStatusBar from './components/UdaciStatusBar';
+import EntryDetail from './components/EntryDetail';
 
 const Tabs = TabNavigator({
   History: {
@@ -45,13 +46,28 @@ const Tabs = TabNavigator({
   }
 });
 
+const MainNavigator = StackNavigator({
+  Home: {
+    screen: Tabs,
+  },
+  EntryDetail: {
+    screen: EntryDetail,
+    navigationOptions: {
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: purple,
+      }
+    }
+  }
+});
+
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{flex: 1}}>
           <UdaciStatusBar backgroundColor={purple} barStyle='light-content' />
-          <Tabs />
+          <MainNavigator />
         </View>
       </Provider>
     );
