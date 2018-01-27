baa88a870916aa7a4f62bbf372e5c8f5338c531b
Luiz Claudio <luizcns18@gmail.com>
2018-01-27 11:13:02 -0300

    feat: Add animation when there are direction changes

--- components/Live.js
+++ components/Live.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet, Animated } from 'react-native';
 import { Foundation } from '@expo/vector-icons';
 import { purple, white } from '../utils/colors';
 import { Location, Permissions } from 'expo';
@@ -10,6 +10,7 @@ export default class Live extends Component {
     coords: null,
     status: null,
     direction: '',
+    bounceValue: new Animated.Value(1),
   };
 
   componentDidMount(){
@@ -50,16 +51,23 @@ export default class Live extends Component {
         const newDirection = calculateDirection(coords.heading);
         const { direction, bounceValue } = this.state;
 
+        if (newDirection !== direction){
+          Animated.sequence([
+            Animated.timing(bounceValue, { duration: 200, toValue: 1.04}),
+            Animated.spring(bounceValue, { toValue: 1, friction: 4})
+          ]).start();
+        }
+
         this.setState(() => ({
           coords,
           status: 'granted',
           direction: newDirection,
-        }))
+        }));
       });
   };
 
   render(){
-    const { status, coords, direction } = this.state;
+    const { status, coords, direction, bounceValue } = this.state;
 
     if(status === null){
       return <ActivityIndicator style={{marginTop: 30}} />
@@ -98,9 +106,11 @@ export default class Live extends Component {
           <Text style={styles.header}>
             You're heading
           </Text>
-          <Text style={styles.direction}>
-            North
-          </Text>
+          <Animated.Text 
+            style={[styles.direction, {transform: [{scale: bounceValue}]}]}
+          >
+            {direction}
+          </Animated.Text>
         </View>
         <View style={styles.metricContainer}>
           <View style={styles.metric}>
